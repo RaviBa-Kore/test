1e93aa134cc8b379376d540c1b197585d79c1406
RaviBa-Kore <68269739+RaviBa-Kore@users.noreply.github.com>
2024-06-05 15:47:58 +0530

    Update node.js

--- node.js
+++ node.js
@@ -3,7 +3,7 @@ const { trace } = require('@opentelemetry/api');
 const tracer = trace.getTracer('dice-lib');
 
 function generateRandom10DigitNumber() {
-    const min = 1000000000; // Smallest 10-digit number
+    const min = 1000000000 ; // Smallest 10-digit number
     const max = 9999999999; // Largest 10-digit number
     return Math.floor(Math.random() * (max - min + 1)) + min ;
 }
