93e34fef7ce22e75882277d38e5746dd369bbc93
RaviBa-Kore <68269739+RaviBa-Kore@users.noreply.github.com>
2024-06-05 14:11:38 +0530

    Update node.js

--- node.js
+++ node.js
@@ -5,7 +5,7 @@ const tracer = trace.getTracer('dice-lib');
 function generateRandom10DigitNumber() {
     const min = 1000000000; // Smallest 10-digit number
     const max = 9999999999; // Largest 10-digit number
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min ;
 }
 
 function tokensGeneration() {
